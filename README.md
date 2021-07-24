726e5933b5eff9cd675267cf85c6bf4f18f2647e
elwood-berry <eberry@anterasoftware.com>
2021-07-24 16:47:53 -0500

    3 files modified, 2 deleted

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Elwood: CORE  
 This is a baseline angular project used for all demo applications developed by Elwood Berry.  
 
-# Log  
+# Log : TASKS  
 
 ## ANGULAR.JSON  
 Modifcations made to this project's [Angular workspace configuration](https://angular.io/guide/workspace-config).  
 
-Usee SASS and prefix everything with 'elwood'
+### 2021.07.23  
+1. Everytime a new component is created, it uses a '.scss' stylesheet file. Usee SASS and prefix everything with 'elwood'
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { NewTestComponent } from './new-test/new-test.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, NewTestComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
